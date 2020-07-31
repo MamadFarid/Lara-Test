c41f31693a6459408f372d0f3ed7541a8d263964
Ariffudin <ariffudinnotresponding@gmail.com>
2020-07-31 17:33:59 +0700

    Test

---
+++ database/migrations/2019_03_16_094709_create_hafalans_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHafalansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hafalan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('kelas_id');
+            $table->unsignedBigInteger('ustadz_id');
+            $table->unsignedBigInteger('santri_id');
+            $table->text('log_hafalan');
+            $table->timestamps();
+
+            $table->foreign('kelas_id')->references('id')->on('kelas')
+                ->onDelete('cascade');
+            $table->foreign('ustadz_id')->references('id')->on('ustadz')
+                ->onDelete('cascade');
+            $table->foreign('santri_id')->references('id')->on('santri')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hafalans');
+    }
+}
